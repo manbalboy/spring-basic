b8ad5867587f0ec858c02cc8851a93bd8675a9c5
manbalboy <manbalboy@hanmail.net>
2022-05-09 22:39:36 +0900

    관심사의 분리와 MVC패턴 실습

--- ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -14,17 +14,19 @@ public class YoilTeller {
 
     @RequestMapping("/getYoil")
     public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        //1.입력
+        // 1. 입력
+//        String year = args[0];
+//        String month = args[1];
+//        String day = args[2];
         String year = request.getParameter("year");
         String month = request.getParameter("month");
         String day = request.getParameter("day");
 
-
-        // 작업
         int yyyy = Integer.parseInt(year);
         int mm = Integer.parseInt(month);
         int dd = Integer.parseInt(day);
 
+        // 2. 처리
         Calendar cal = Calendar.getInstance();
         cal.set(yyyy, mm - 1, dd);
 
@@ -32,14 +34,20 @@ public class YoilTeller {
         char yoil = " 일월화수목금토".charAt(dayOfWeek);
 
         // 3. 출력
-        System.out.println(year + "년 " + month + "월 " + day + "일은");
-        System.out.println(yoil + "요일입니다.");
-
-        response.setContentType("text/html");
-        response.setCharacterEncoding("utf-8");
-        PrintWriter out = response.getWriter();
-        out.println(year + "년 " + month + "월 " + day + "일은");
+//        System.out.println(year + "년 " + month + "월 " + day + "일은 ");
+//        System.out.println(yoil + "요일입니다.");
+        response.setContentType("text/html");    // 응답의 형식을 html로 지정
+        response.setCharacterEncoding("utf-8");  // 응답의 인코딩을 utf-8로 지정
+        PrintWriter out = response.getWriter();  // 브라우저로의 출력 스트림(out)을 얻는다.
+        out.println("<html>");
+        out.println("<head>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println(year + "년 " + month + "월 " + day + "일은 ");
         out.println(yoil + "요일입니다.");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
 
     }
 }
---
+++ ch2/src/main/webapp/WEB-INF/views/yoil.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jeonghun
+  Date: 2022/05/09
+  Time: 10:16 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+<p>${year} 년 ${month} 월 ${day}일은 ${yoil} 입니다.</p>
+</body>
+</html>
