f5b362a3eb618ebbd76dac22351fff9f555b4f23
manbalboy <manbalboy@hanmail.net>
2022-05-10 23:18:13 +0900

    mvc 패턴 원리 확인 예제

--- ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -4,30 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
 import java.util.Calendar;
 
 @Controller
 public class YoilTellerMVC {
+    @RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC
+    public String main(int year, int month, int day, Model model) {
 
-    @RequestMapping("/getYoilMVC")
-    public String main(int year,int month, int day, Model model) throws IOException {
-        if(!isValid(year, month, day)) return "yoilError";
+        // 1. 유효성 검사
+        if(!isValid(year, month, day))
+            return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
 
         // 2. 처리
         char yoil = getYoil(year, month, day);
 
+        // 3. Model에 작업 결과 저장
         model.addAttribute("year", year);
         model.addAttribute("month", month);
         model.addAttribute("day", day);
         model.addAttribute("yoil", yoil);
 
-        return "yoil";
-
-    }
-
-    private boolean isValid(int year, int month, int day) {
-        return true;
+        // 4. 작업 결과를 보여줄 View의 이름을 반환
+        return "yoil"; // /WEB-INF/views/yoil.jsp
     }
 
     private char getYoil(int year, int month, int day) {
@@ -36,5 +34,12 @@ public class YoilTellerMVC {
 
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
         return " 일월화수목금토".charAt(dayOfWeek);
+    }
+
+    private boolean isValid(int year, int month, int day) {
+        if(year==-1 || month==-1 || day==-1)
+            return false;
+
+        return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크
     }
 }
